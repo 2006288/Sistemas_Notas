7b2a3454fdd73aecbb0dad5fde278776d074acfe
2006288 <anthonyescuela@gmail.com>
2024-04-01 15:27:55 -0500

    Las tres primeras pestañas rediseñadas y agragada la pestaña desempeño de los estudiantes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 window.addEventListener("resize", anchoPagina);
 
 var contenedorInformacion = document.getElementById("contenedorInformacion");
+var menulateral = document.getElementById("sidebar");
 
 function anchoPagina(){
     var anchoVentana = window.innerWidth;
+    var altoVentana = window.innerHeight;
     if(window.innerWidth > 850){
-        let resta = anchoVentana-80;
-        let restactiva = anchoVentana-350;
-        contenedorInformacion.setAttribute("style",`--normal:${resta}px; --activo:${restactiva}px;`);
+        let resta = anchoVentana-86;
+        let restactiva = anchoVentana-356;
+        contenedorInformacion.setAttribute("style",`--normal:${resta}px; --activo:${restactiva}px; --alto:${altoVentana}px;`);
+        menulateral.setAttribute("style",`--alto:${altoVentana}px;`);
     }else{
-        contenedorInformacion.setAttribute("style",`--normal:${anchoVentana}px;`);
+        contenedorInformacion.setAttribute("style",`--normal:${anchoVentana}px; --alto:${altoVentana}px;`);
+        menulateral.setAttribute("style",`--alto:${altoVentana}px; --normal:${anchoVentana}px;`);
     }
 }
 
